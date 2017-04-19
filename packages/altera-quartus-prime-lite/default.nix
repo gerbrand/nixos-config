810134a789da3d4a7d2bebc09677654c0d5c3ec9
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-04-19 20:54:31 +0200

    altera-quartus-prime-lite: fix Desktop file

--- packages/altera-quartus-prime-lite/default.nix
+++ packages/altera-quartus-prime-lite/default.nix
@@ -1,8 +1,3 @@
-# TODO: eclipse-nios2 (and probably others) do not find libstdc++.so.6
-#
-# TODO: (follow up) eclipse-nios2 fails to start: it cannot load SWT library
-#        due to missing libgtk-x11-2.0.so.0
-
 { stdenv, fetchurl, utillinux, file, bash, glibc, pkgsi686Linux, writeScript
 , nukeReferences
 # Runtime dependencies
@@ -13,6 +8,11 @@
 #   quartus_update modelsim_ase modelsim_ae
 , disableComponents ? []
 }:
+
+# TODO: eclipse-nios2 (and probably more) do not find libstdc++.so.6
+#
+# TODO: (follow up on the above) eclipse-nios2 fails to start: it cannot load
+# SWT library due to missing libgtk-x11-2.0.so.0
 
 let
   disableComponentsOption =
@@ -230,7 +230,7 @@ stdenv.mkDerivation rec {
     Name=Quartus Prime ${version} Lite Edition
     Comment=Quartus Prime ${version}
     Icon=$out/quartus/adm/quartusii.png
-    Exec=$out/quartus/bin/quartus
+    Exec=$out/bin/quartus
     Terminal=false
     Path=$out
     EOF
